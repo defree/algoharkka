5a15bf9059ec69a59dbe0ddd5b9839e581f738bd
Esa Aaltonen <estaalt@utu.fi>
2015-10-07 02:12:28 +0300

    eka

---
+++ Assets/Scripts/Pathfinding/Cell.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+
+	public Vector2 coordinates;
+	private bool walkable = true;
+	private float movementCost = 0;
+
+
+	public Cell (int x, int y) {
+
+		this.coordinates.x = x;
+		this.coordinates.y = y;
+
+	}
+
+	public void setWalkable(bool walkable) {
+		this.walkable = walkable;
+	}
+
+	public bool IsWalkable() {
+		return walkable;
+	}
+
+	public float MovementCost() {
+		return movementCost;
+	}
+}
